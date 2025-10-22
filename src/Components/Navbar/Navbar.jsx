c15a2e48f163f3f8e88e61e4b49c5c17c495d5ed
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-10-22 15:56:31 +0200

    add navbar component

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,14 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import "./header.css";
 import { Link, NavLink } from "react-router-dom";
 import { CiSearch } from "react-icons/ci";
 import { useCookies } from "react-cookie";
+import { contextData } from "../../Context/Context";
 
 export default function Navbar() {
   const [cookies, removeCookie] = useCookies(["token"]);
-  const userID = cookies?.token?.data?.user?.id;
-  const token = cookies?.token?.data?.token;
-  // بنجيب بيانات المستخدم من التوكن اللي في الكوكيز
-  const [userData, setUserData] = useState({});
-  console.log(userData);
+  const { userID, token, fetchUserData, userData } = useContext(contextData);
+  const isLoggedIn = Boolean(token && token !== "undefined");
   const [showToast, setShowToast] = useState(true);
 
   // حالة لإظهار أو إخفاء كارت البروفايل لما نضغط على الصورة
@@ -24,25 +22,7 @@ export default function Navbar() {
   const menuRef = useRef(null);      // تمثل قائمة الروابط (ul)
   const toggleRef = useRef(null);    // تمثل زر فتح المينيو
   const mobileProfileRef = useRef(null);
-  const desktopProfileRef = useRef(null);   // تمثل صورة أو زر البروفايل
-
-  const inputRef = useRef(null);   // الضغط علي البحث يوجهك لل input
-
-  const handleFocus = () => {
-    inputRef.current.focus();
-  }
-
-  // useEffect الأول: يقفل المينيو أو كارت البروفايل لما نضغط على زر Escape
-  useEffect(() => {
-    const onKey = (e) => {
-      if (e.key === "Escape") {
-        setMenuOpen(false);
-        setToggleProfileCard(false);
-      }
-    };
-    document.addEventListener("keydown", onKey);
-    return () => document.removeEventListener("keydown", onKey);
-  }, []);
+  const desktopProfileRef = useRef(null);   // تمثل صورة أو زر البروفاي
 
   useEffect(() => {
     const handleClickOutside = (e) => {
@@ -61,174 +41,109 @@ export default function Navbar() {
 
   // useEffect الثاني: يقفل المينيو لما نضغط براها
   useEffect(() => {
-    if (!menuOpen) return; // لو المينيو مش مفتوحة، متسمعش للحدث
+    if (!menuOpen) return;
 
     const handleClickOutside = (e) => {
       const target = e.target;
-
-      // لو النقرة داخل المينيو نفسها، متقفلهاش
       if (menuRef.current && menuRef.current.contains(target)) {
         return;
       }
-
-      // لو النقرة كانت على زر الفتح، متقفلهاش برضو
       if (toggleRef.current && toggleRef.current.contains(target)) {
         return;
       }
-
-      // لو الضغط في أي مكان تاني -> اقفل المينيو
       setMenuOpen(false);
     };
-
-    // بنضيف مستمع للضغط على أي مكان في الصفحة
     document.addEventListener("mousedown", handleClickOutside);
 
-    // بنشيله لما الكومبوننت يتفكك أو المينيو تتقفل
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [menuOpen]);
 
   useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const response = await fetch(`https://api.mashy.sand.alrmoz.com/api/user/${userID}`, {
-          method: "GET",
-          headers: { Authorization: `Bearer ${token}` }
-        });
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        setUserData(data.data);
-      } catch (err) {
-        console.log(err.message);
-      }
-    };
-
     if (userID && token) {
       fetchUserData();
     }
   }, [userID, token]);
 
   return (
-    <header className="header">
-      <div className="header-container">
-
+    <header className="navbar">
+      <div className="navbar_container">
         {/* اللوجو */}
         <div className="logo">
           <NavLink to="/" onClick={closeMenu}>
             <img src="/images/logo.svg" alt="logo" />
           </NavLink>
         </div>
+        {/* قائمة الروابط الرئيسية */}
+        <ul id="primary-navigation" ref={menuRef} className={`nav ${menuOpen ? "open" : ""}`} >
+        </ul>
 
-        <div className="mobile-search">
-          <CiSearch className="search_icon" onClick={handleFocus} />
-          <input type="search" placeholder="ابحث هنا..." ref={inputRef} />
-        </div>
+        <div className="navbar_buttons">
+          {/* desktop */}
+          <div className="desktop_buttons">
+            {isLoggedIn ? (
+              <button type="button" onClick={() => setToggleProfileCard(!toggleProfileCard)} className="btn_profile" ref={desktopProfileRef}>
+                <span>حسابي</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-down" ><path d="m6 9 6 6 6-6" /></svg>
+              </button>
+            ) : (
+              <NavLink to="/login" className="login_link">
+                <span>تسجيل الدخول</span>
+              </NavLink>
+            )}
 
-        {/* الجزء الخاص بالموبايل (زر المينيو + صورة البروفايل أو تسجيل الدخول) */}
-        <div className="menu-mobile-toggle">
+            <NavLink to="/Advertisements" className="advertisements_buttom">
+              <span>اضف عرضك</span>
+              <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-plus"><path d="M5 12h14" /><path d="M12 5v14" /></svg>
+            </NavLink>
+          </div>
 
-          {/* لو المستخدم مسجل دخول */}
-          <div className="mobile-login">
-            {Boolean(token) ? (
-              <div>
-                {/* صورة أو أول حرفين من الاسم */}
-                <Link
-                  type="button"
+          <div className="menu-mobile-toggle">
+            {/* لو المستخدم مسجل دخول */}
+            <div className="mobile-login">
+              {isLoggedIn ? (
+                <div
+                  role="button"
                   onClick={() => setToggleProfileCard(!toggleProfileCard)}
                   className="header_profile_img"
                   ref={mobileProfileRef}
                 >
                   {userData?.profile_image === null ? (
-                    <span className="two_char">
-                      <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-circle-user-round-icon lucide-circle-user-round"><path d="M18 20a6 6 0 0 0-12 0" /><circle cx={12} cy={10} r={4} /><circle cx={12} cy={12} r={10} /></svg>
-                    </span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-circle-user-round-icon lucide-circle-user-round"><path d="M18 20a6 6 0 0 0-12 0" /><circle cx={12} cy={10} r={4} /><circle cx={12} cy={12} r={10} /></svg>
                   ) : (
                     <img src={userData?.profile_image} alt={userData?.name?.split(" ").map((word) => word[0]).join(" ").toUpperCase()} className="user_img" />
                   )}
-                </Link>
-
-                {/* كارت البروفايل */}
-                <ProfileCard
-                  toggleProfileCard={toggleProfileCard}
-                  userData={userData}
-                  removeCookie={removeCookie}
-                />
-              </div>
-            ) : (
-              <NavLink to="/login" className="mobile_loginBTN">
-                <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-log-in">
-                  <path d="m10 17 5-5-5-5" />
-                  <path d="M15 12H3" />
-                  <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
-                </svg>
-              </NavLink>
-            )}
-          </div>
-
-          <div
-            ref={toggleRef}
-            className="menu-toggle"
-            onClick={() => setMenuOpen((prev) => !prev)}
-            aria-expanded={menuOpen}
-            aria-controls="primary-navigation"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-text-align-justify" ><path d="M3 5h18" /><path d="M3 12h18" /> <path d="M3 19h18" /></svg>
-          </div>
-        </div>
-
-        {/* قائمة الروابط الرئيسية */}
-        <ul
-          id="primary-navigation"
-          ref={menuRef}
-          className={`nav ${menuOpen ? "open" : ""}`}
-        >
-        </ul>
-
-        {/* أزرار الهيدر في الديسكتوب */}
-        <div className="header-button">
-          {cookies?.token?.data?.token && cookies?.token?.data?.token !== "undefined" ? (
-            <div>
-              {/* زر البروفايل في الديسكتوب */}
-              <Link
-                type="button"
-                onClick={() => setToggleProfileCard(!toggleProfileCard)}
-                className="btn_profile" ref={desktopProfileRef}
-              >
-                <span>حسابي</span>
-                <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-down" ><path d="m6 9 6 6 6-6" /></svg>
-              </Link>
-
-              {/* كارت البروفايل */}
-              <ProfileCard
-                toggleProfileCard={toggleProfileCard}
-                userData={userData}
-                removeCookie={removeCookie}
-              />
+                </div>
+              ) : (
+                <NavLink to="/login" className="mobile_loginBTN">
+                  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-log-in"><path d="m10 17 5-5-5-5" /><path d="M15 12H3" /><path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" /></svg>
+                </NavLink>
+              )}
             </div>
-          ) : (
-            // زر تسجيل الدخول لو مش داخل
-            <NavLink to="/login" className="btn-delete">
-              <span>تسجيل الدخول</span>
-            </NavLink>
-          )}
 
-          <NavLink to="/Advertisements" className="btn-add">
-            <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round"className="lucide lucide-plus"><path d="M5 12h14" /><path d="M12 5v14" /></svg>
-            <span>اضف عرضك</span>
-          </NavLink>
+            <div
+              ref={toggleRef}
+              className="menu_toggle"
+              onClick={() => setMenuOpen((prev) => !prev)}
+              aria-expanded={menuOpen}
+              aria-controls="primary-navigation"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-text-align-justify" ><path d="M3 5h18" /><path d="M3 12h18" /> <path d="M3 19h18" /></svg>
+            </div>
+          </div>
+          {/* كارت البروفايل */}
+          {isLoggedIn && (<ProfileCard toggleProfileCard={toggleProfileCard} userData={userData} removeCookie={removeCookie} />)}
         </div>
       </div>
-      {Boolean(token) && showToast && userData?.area === null && (<ToastWarning message="الرجاء إضافة الموقع قبل المتابعة." onClose={() => setShowToast(false)} />)}
+      {isLoggedIn && userData?.area === null && (<ToastWarning message="الرجاء إضافة الموقع قبل المتابعة." onClose={() => setShowToast(false)} />)}
     </header>
   );
 };
 
 export function ProfileCard({ toggleProfileCard, userData, removeCookie }) {
   return (
-    <div className="profile-card" style={{ height: toggleProfileCard ? "300px" : "0" }}>
+    <div className="profile_card" style={{ height: toggleProfileCard ? "280px" : "0" }}>
       <div className="user-info">
         {userData?.profile_image === null ? (
           <span className="two_char">
@@ -239,7 +154,7 @@ export function ProfileCard({ toggleProfileCard, userData, removeCookie }) {
         )}
         <div>
           <p className="greeting">أهلا</p>
-          <p className="username">{userData?.name}</p>
+          <p className="username">{userData?.name?.split(" ").slice(0, 2).join(" ")}</p>
         </div>
       </div>
       <Link to="/accountUser" className="show_accountUser"><span>عرض الملف الشخصي</span></Link>
@@ -248,13 +163,8 @@ export function ProfileCard({ toggleProfileCard, userData, removeCookie }) {
         <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-settings-icon lucide-settings"><path d="M9.671 4.136a2.34 2.34 0 0 1 4.659 0 2.34 2.34 0 0 0 3.319 1.915 2.34 2.34 0 0 1 2.33 4.033 2.34 2.34 0 0 0 0 3.831 2.34 2.34 0 0 1-2.33 4.033 2.34 2.34 0 0 0-3.319 1.915 2.34 2.34 0 0 1-4.659 0 2.34 2.34 0 0 0-3.32-1.915 2.34 2.34 0 0 1-2.33-4.033 2.34 2.34 0 0 0 0-3.831A2.34 2.34 0 0 1 6.35 6.051a2.34 2.34 0 0 0 3.319-1.915" /><circle cx={12} cy={12} r={3} /></svg>
         <span>إعدادات الحساب</span>
       </Link>
-      <button
-        className="logout-btn"
-        onClick={() => removeCookie("token")}
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-log-out-icon lucide-log-out">
-          <path d="m16 17 5-5-5-5" /> <path d="M21 12H9" /> <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
-        </svg>
+      <button className="logout-btn" onClick={() => removeCookie("token")} >
+        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-log-out-icon lucide-log-out"><path d="m16 17 5-5-5-5" /> <path d="M21 12H9" /> <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" /> </svg>
         <span>تسجيل الخروج</span>
       </button>
     </div>
