5b2458b5a361290812fc9625cd10f6914659e6ac
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-10-25 19:06:37 +0200

    Add register page

--- src/Pages/Auth/Register/Register.jsx
+++ src/Pages/Auth/Register/Register.jsx
@@ -191,7 +191,7 @@ export default function Register() {
                     onChange={formik.handleChange}
                     onBlur={formik.handleBlur}
                   />
-                  <div className="eye_icon" onClick={() => setShowPassword(!showPassword)}>
+                  <div className="eye_password_icon" onClick={() => setShowPassword(!showPassword)}>
                     {showPassword ?
                       <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-eye-icon lucide-eye"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" /><circle cx={12} cy={12} r={3} /></svg>
                       :
