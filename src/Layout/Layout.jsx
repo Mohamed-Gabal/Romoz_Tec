15410af6fd077fd7fc3d8464cd9138cda5eed924
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-10-29 22:39:08 +0200

    Add layoutStyle.css

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Footer from "../Components/Footer/Footer";
-import "./MainLayout.css";
+import "./layoutStyle.css";
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
 
